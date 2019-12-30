00c8eb9b4d0b7e2610e603492346091679252414
SeongChoi <choies921003@gmail.com>
2019-12-30 15:40:50 -0800

    added redux boilerplate

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import { Provider } from 'react-redux';
+import store from './store'
 import Root from './Root';
 
-render(<Root/>, document.getElementById('root'));
+render(
+    <Provider store={store}>
+        <Root />
+    </Provider>,
+    document.getElementById('root')
+);
---
+++ src/public/reducers/musicReducer.js
@@ -0,0 +1,13 @@
+import * as types from '../constants/actionTypes'
+
+const initialState = {
+
+}
+
+const musicReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+    }
+}
+
+export default musicReducer;
