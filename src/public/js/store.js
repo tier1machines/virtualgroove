6e617e1a820f83a38ecbd69a416d768b1bd03fb8
SeongChoi <choies921003@gmail.com>
2019-12-31 11:59:29 -0800

    testing

--- src/public/js/store.js
+++ src/public/js/store.js
@@ -2,8 +2,13 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from '../reducers/musicReducer';
 
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
+
 const store = createStore(
     reducers,
+    applyMiddleware(thunkMiddleware),
     composeWithDevTools()
 );
 
