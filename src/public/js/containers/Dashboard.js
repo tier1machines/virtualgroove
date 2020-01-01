6425f0dcbd071bcab9e95cefb32f526571758769
SeongChoi <choies921003@gmail.com>
2019-12-31 17:43:11 -0800

    added login

--- src/public/js/containers/Dashboard.js
+++ src/public/js/containers/Dashboard.js
@@ -1,53 +1,77 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 /* Components */
 import Player from './Player';
 import Controls from './Controls';
+import Login from './Login';
 
 class Dashboard extends Component {
 
-  constructor() {
-	super();
-	this.state = {
-	  currentVideo: '',
-	  index: '',
-	  status: '',
-	  queue: [],
-	}
-  };
-
-  addTrack = e => {
-	if (!this.state.queue.length) {
-	  this.setState({ currentVideo : e.id.videoId });
+	constructor() {
+		super();
+		this.state = {
+			currentVideo: '',
+			index: '',
+			status: '',
+			queue: [],
+			//
+			id: '',
+			password: '',
+			loginSuccess: false
+			//
+		}
 	};
-	const queueCopy = Object.assign([], this.state.queue);
-	queueCopy.push(e);
-	this.setState({ queue: queueCopy });
-  };
+	//
+	onChangeId = id => {
+		this.setState({ id: id })
+	}
+	onChangePassword = password => {
+		this.setState({ password: password })
+	}
+	handleLogin = () => {
+		axios.post('/auth/login', {
+			username: this.state.id,
+			password: this.state.password
+		})
+			.then(resposnse => {
+				console.log('response', resposnse)
+			})
+	}
+	//
 
-  setCurrentVideo = (selectedVideo, index) => {
-	this.setState({ currentVideo: selectedVideo.id.videoId, index });
-  };
+	addTrack = e => {
+		if (!this.state.queue.length) {
+			this.setState({ currentVideo: e.id.videoId });
+		};
+		const queueCopy = Object.assign([], this.state.queue);
+		queueCopy.push(e);
+		this.setState({ queue: queueCopy });
+	};
 
-  setStatus = status => {
-	this.setState({ status });
-  };
+	setCurrentVideo = (selectedVideo, index) => {
+		this.setState({ currentVideo: selectedVideo.id.videoId, index });
+	};
 
-  removeVideo = index => {
-	console.log('Removing video');
-	const queueCopy = Object.assign([], this.state.queue);
-	queueCopy.splice(index, 1);
-	this.setState({ queue: queueCopy });
-  };
+	setStatus = status => {
+		this.setState({ status });
+	};
 
-  render() {
-	return (
-	  <div id='dashboard'>
-		<Player currentVideo={this.state.currentVideo} setStatus={this.setStatus} setCurrentVideo={this.setCurrentVideo} queue={this.state.queue} />
-		<Controls queue={this.state.queue} addTrack={this.addTrack} setCurrentVideo={this.setCurrentVideo} status={this.state.status} removeVideo={this.removeVideo} />
-	  </div>
-	)
-  }
+	removeVideo = index => {
+		console.log('Removing video');
+		const queueCopy = Object.assign([], this.state.queue);
+		queueCopy.splice(index, 1);
+		this.setState({ queue: queueCopy });
+	};
+
+	render() {
+		return (
+			<div id='dashboard'>
+				<Player currentVideo={this.state.currentVideo} setStatus={this.setStatus} setCurrentVideo={this.setCurrentVideo} queue={this.state.queue} />
+				<Controls queue={this.state.queue} addTrack={this.addTrack} setCurrentVideo={this.setCurrentVideo} status={this.state.status} removeVideo={this.removeVideo} />
+			</div>
+		)
+	}
 }
 
 export default Dashboard;
--- src/public/js/containers/Navbar.js
+++ src/public/js/containers/Navbar.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
-
-  render() {
-	return (
-	  <div id='navbar'>
-		<p>About</p>
-		<p>Virtual Groove</p>
-		<p>Log Out</p>
-	  </div>
-	)
-  }
+	render() {
+		return (
+			<div id='navbar'>
+				<p>About</p>
+				<p>Virtual Groove</p>
+				<p>Log Out</p>
+			</div>
+		)
+	}
 
 }
 
